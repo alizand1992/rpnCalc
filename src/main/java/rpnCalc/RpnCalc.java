f28b1513f2c89ff7671226106fa546ab75287fe7
Ali Zand <ali.zand9224@gmail.com>
2018-08-27 18:29:03 -0700

    RPN-3 Implement and test inputs

--- src/main/java/rpnCalc/RpnCalc.java
+++ src/main/java/rpnCalc/RpnCalc.java
@@ -1,5 +1,28 @@
 package com.rpnCalc;
 
-public class RpnCalc {
+import com.rpnCalc.InputQueue;
 
+public class RpnCalc {
+    private double result;
+    private InputQueue inputs;
+
+    public RpnCalc() {
+        this("");
+    }
+
+    public RpnCalc(String input) {
+        inputs = new InputQueue(input);
+    }
+
+    public void addInput(String input) {
+
+    }
+
+    public void calculate() {
+
+    }
+
+    public double getResult() {
+        return result;
+    }
 }
--- src/test/java/rpnCalc/RpnCalcTest.java
+++ src/test/java/rpnCalc/RpnCalcTest.java
@@ -3,9 +3,123 @@ package com.rpnCalc;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class RpnCalcTest {
+    private static final double DELTA = 1e-15;
 
+    //    @Ignore
+    @Test
+    public void emptyConstructorHasResultOfZero() {
+        RpnCalc rpn = new RpnCalc();
+        assertEquals(0.0, rpn.getResult(), DELTA);
+    }
+
+    //    @Ignore
+    @Test
+    public void emptyConstructorHasInputsSetToNull() {
+        try {
+            RpnCalc rpn = new RpnCalc();
+            Field field = rpn.getClass().getDeclaredField("inputs");
+            field.setAccessible(true);
+            InputQueue inputs = (InputQueue)field.get(rpn);
+            assertNotNull(inputs);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    //    @Ignore
+    @Test
+    public void constructorWithInputAddsToTheInputs() {
+        try {
+            RpnCalc rpn = new RpnCalc("1 2 3");
+            Field field = rpn.getClass().getDeclaredField("inputs");
+            field.setAccessible(true);
+            InputQueue inputs = (InputQueue)field.get(rpn);
+            assertEquals(3, inputs.size());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    // @Ignore
+    @Test
+    public void constructorWithInputAddsAllTokensInOrder() {
+        try {
+            RpnCalc rpn = new RpnCalc("1 2 3");
+            Field field = rpn.getClass().getDeclaredField("inputs");
+            field.setAccessible(true);
+            InputQueue inputs = (InputQueue)field.get(rpn);
+            assertEquals("1", inputs.pop());
+            assertEquals("2", inputs.pop());
+            assertEquals("3", inputs.pop());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    //    @Ignore
+    @Test
+    public void addInputCreatesInputButDoesNothingForEmptyInput() {
+        RpnCalc rpn = new RpnCalc();
+        rpn.addInput("");
+        assertEquals(0.0, rpn.getResult(), DELTA);
+        try {
+            Field field = rpn.getClass().getDeclaredField("inputs");
+            field.setAccessible(true);
+            InputQueue inputs = (InputQueue)field.get(rpn);
+            assertNotNull(inputs);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    // @Ignore
+    @Test
+    public void addInputAddsAsManyAsThereAreTokens() {
+        RpnCalc rpn = new RpnCalc();
+        rpn.addInput("1 2 3");
+        try {
+            Field field = rpn.getClass().getDeclaredField("inputs");
+            field.setAccessible(true);
+            InputQueue inputs = (InputQueue)field.get(rpn);
+            assertEquals("1", inputs.pop());
+            assertEquals("2", inputs.pop());
+            assertEquals("3", inputs.pop());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            fail();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void calculateDoesNothingWhenThereAreNoInputs() {
+        RpnCalc rpn = new RpnCalc();
+        rpn.calculate();
+        assertEquals(0.0, rpn.getResult(), DELTA);
+    }
 }
